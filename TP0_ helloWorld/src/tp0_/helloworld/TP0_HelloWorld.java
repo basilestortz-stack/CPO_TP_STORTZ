10df97b1c1d5ac580f63914e5c88328f234cd811
basilestortz <basilestortz@10.3.1.186>
2025-11-19 12:32:56 +0100

    TP1 complété

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**20oct2025stortz
+ *
+ * @author basilestortz
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //System.out.println( " Bonjour " ) ;
+    String prenom;
+    Scanner sc ;
+    sc = new Scanner (System.in) ;
+    System.out.println("Quel est votre prénom ?");
+    prenom = sc.nextLine();
+    }
+    
+}
