805fd916406fc81043cfc57fab915cb18bfbc7bd
basilestortz <basilestortz@10.3.1.186>
2025-11-26 15:31:23 +0100

    TP quiz foot

--- TP2_Bieres_STORTZ/TP_Quiz_STORTZ/src/Question.java
+++ TP2_Bieres_STORTZ/TP_Quiz_STORTZ/src/Question.java
@@ -8,24 +8,31 @@
  * @author basilestortz
  */
 public class Question {
+
+    // Attributs privés
     private String intitule;
     private String proposition1;
     private String proposition2;
     private String proposition3;
     private String proposition4;
-    private int indexBonneReponse;
-    
+    private int indexBonneReponse; // compris entre 1 et 4
+
+    // Constructeur pour initialiser tous les attributs
     public Question(String intitule, String p1, String p2, String p3, String p4, int indexBonneReponse) {
         this.intitule = intitule;
         this.proposition1 = p1;
         this.proposition2 = p2;
         this.proposition3 = p3;
         this.proposition4 = p4;
+
+        // Vérification que l'index de la bonne réponse est correct
+        if(indexBonneReponse < 1 || indexBonneReponse > 4) {
+            throw new IllegalArgumentException("L'index de la bonne réponse doit être compris entre 1 et 4.");
+        }
         this.indexBonneReponse = indexBonneReponse;
     }
 
-    // --- Accesseurs (getters) ---
-
+    // Accesseurs (getters) pour l'intitulé et les propositions
     public String getIntitule() {
         return intitule;
     }
@@ -46,7 +53,10 @@ public class Question {
         return proposition4;
     }
 
+    // Accesseur pour l'index de la bonne réponse
     public int getIndexBonneReponse() {
         return indexBonneReponse;
     }
 }
+    
+
